797da4c51f0977afe562cba56db6edbccde24c80
Junyeop Kim <junykim@c8r8s7.42seoul.kr>
2022-12-29 11:15:40 +0900

    second

--- cmd_builtin.c
+++ cmd_builtin.c
@@ -1,36 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   cmd_builtin.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: junykim <junykim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/28 13:41:12 by junykim           #+#    #+#             */
-/*   Updated: 2022/12/28 15:59:29 by junykim          ###   ########.fr       */
+/*   Created: 2022/12/28 15:24:31 by junykim           #+#    #+#             */
+/*   Updated: 2022/12/28 15:24:40 by junykim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
 
-// free, malloc
-# include <stdlib.h>
-
-// access
-#include <unistd.h>
-
-# include "libft/libft.h"
-
-typedef enum e_cmd
-{
-	DOUBLE_PIPE,
-	DOUBLE_AMPER,
-	SINGLE_CMD,
-	SINGLE_PIPE
-}t_cmd;
-
-// ================================
-//			cmd1.c
-// ================================
-char	*get_cmd(char **paths, char *cmd);
-#endif
